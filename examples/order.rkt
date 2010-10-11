23d69dba9f52c2c8d487c2147d90f8a2a1600229
Jay McCarthy <jay.mccarthy@gmail.com>
2010-10-11 14:53:47 -0600

    fix

--- examples/order.rkt
+++ examples/order.rkt
@@ -2,11 +2,20 @@
 
 #include <stdio.h>
 
+float f( float x1, float x2 ) {
+ return x1 / x2;
+}
+
 int main() {
-  int x = 0.0;
+  int x = 0;
   float r = (++x) / (++x);
   printf("%4.2f\n", r);
+  x = 0;
+  r = f((++x),(++x));
+  printf("%4.2f\n", r);
   r = 1.0 / 2.0;
+  printf("%4.2f\n", r);
+  r = 2.0 / 1.0;
   printf("%4.2f\n", r);
   return 0;
 }
