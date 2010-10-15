deb31962911bcc8ce0ad528b1d1f65c91ad6e7aa
Jay McCarthy <jay.mccarthy@gmail.com>
2010-10-15 11:51:15 -0600

    Using c++ instead of cc

--- runtime.rkt
+++ runtime.rkt
@@ -3,7 +3,7 @@
   (define src-path (make-temporary-file "~a.c"))
   (with-output-to-file src-path (λ () (write-bytes bs)) #:exists 'replace)
   (define exe-path (path-add-suffix src-path #".bin"))
-  (define cc-path (find-executable-path "cc"))
+  (define cc-path (find-executable-path "c++"))
   (system* cc-path (path->string src-path) "-o" (path->string exe-path))
   (system* exe-path))
 (provide run-c-program)
