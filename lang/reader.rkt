33a35d3607f9b1009875051c82200014ef872d89
Jay McCarthy <jay.mccarthy@gmail.com>
2011-08-22 14:15:18 -0600

    Updating to recent Racket

--- lang/reader.rkt
+++ lang/reader.rkt
@@ -3,5 +3,5 @@
   #:read (lambda ([in (current-input-port)]) (read-c-syntax #f in))
   #:read-syntax read-c-syntax
   #:whole-body-readers? #t
-  (require unstable/planet
+  (require planet/version
            (file "../read.rkt")))
