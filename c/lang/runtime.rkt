dbebcda736cceee44596cc8c3efccc11f4e7dfdb
John Clements <clements@racket-lang.org>
2014-12-12 14:36:22 -0800

    signal error when c++ compiler not found

--- c/lang/runtime.rkt
+++ c/lang/runtime.rkt
@@ -6,6 +6,9 @@
   (with-output-to-file src-path (λ () (write-bytes bs)) #:exists 'replace)
   (define exe-path (path-add-suffix src-path #".bin"))
   (define cc-path (find-executable-path "c++"))
-  (system* cc-path (path->string src-path) "-o" (path->string exe-path))
-  (system* exe-path))
+  (cond [(path? cc-path)
+         (system* cc-path (path->string src-path) "-o" (path->string exe-path))
+         (system* exe-path)]
+        [else
+         (error 'run-c "can't find executable path for c++ executable.")]))
 (provide run-c-program)
