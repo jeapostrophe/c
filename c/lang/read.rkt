183b4698c737b9f40386e4ce22d6798b0a1c2a04
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-10-21 13:46:17 -0400

    Fix for new macro system.

--- c/lang/read.rkt
+++ c/lang/read.rkt
@@ -2,5 +2,5 @@
 (require racket/port "runtime.rkt")
 (define (read-c-syntax [source-name #f] [in (current-input-port)])
   (list
-   #`(run-c-program #,(port->bytes in))))
+   `(run-c-program ,(port->bytes in))))
 (provide read-c-syntax)
